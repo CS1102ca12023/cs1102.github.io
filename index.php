0293e9150d1b4b25c5a124bd93ead5d921d48a15
CS1102ca12023 <165821391+CS1102ca12023@users.noreply.github.com>
2024-04-22 13:56:51 +0800

    index.php

--- index.php
+++ index.php
@@ -6,12 +6,15 @@
             display: flex;
             align-items: center;
             justify-content: center;
+            background-color: #86a7db;
+        }
+        .center {
+            text-align: center;
         }
     </style>
 </head>
 <body>
 <?php
-                $title = "index.php";
   echo '<div class="center">';
   echo '    <h1>CS1102 - Course Project</h1>';
   echo '    Lets review your learning process about different image compression algorithams. <br>
@@ -21,10 +24,11 @@
     echo '    <input name="ans" type="text">';
     echo '    <input type="submit">';
     echo '</form>';
-  echo '<a href="https://www.cs.cityu.edu.hk/">Back to Home page</a>';
+  echo '<a href="https://cs1102ca12023.github.io/cs1102.github.io/">Back to Home page</a>';
   echo '</div>';
   echo '</body>';
   echo '</html>';
 ?>
 </body>
 </html>
+
